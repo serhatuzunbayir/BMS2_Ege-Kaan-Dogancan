4c39069d0dedf6480dd049fc1e7eec3038feb507
Doğancan <dhirdavatcioglu@yahoo.com>
2021-06-02 22:40:14 +0300

    RegisterScreen updated

--- RegisterScreen.java
+++ RegisterScreen.java
@@ -1,91 +1,55 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.*;
 
 public class RegisterScreen {
     // CREATING ACCOUNT SCREEN WITH INPUTS OF NAME, USERNAME, PASSWORD, HESCODE
     public static void showRegisterMenu() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
         System.out.println("=====================");
         System.out.println("USER REGISTERATION");
         System.out.println("=====================");
-        var register = new Registration();
-        try(Scanner scanner = new Scanner(System.in)) {
+        try {
+            String sqlQuery = "insert bmsUsers values(?,?,?,?,?,?,?)";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://94.130.57.82/appsplat_semih", "appsplat_semih", "semihsemih123");
+            PreparedStatement ps = con.prepareStatement(sqlQuery);
             System.out.print("Enter your first name: ");
-            String firstName = scanner.nextLine();
-            register.setFirstName(firstName);
+            String firstName = br.readLine();
+            ps.setString(1, firstName);
             System.out.print("Enter your last name: ");
-            String lastName = scanner.nextLine();
-            register.setLastName(lastName);
+            String lastName = br.readLine();
+            ps.setString(2, lastName);
             System.out.print("Enter your user name: ");
-            String username = scanner.nextLine();
-            register.setUsername(username);
+            String username = br.readLine();
+            ps.setString(3, username);
             System.out.print("Enter your password: ");
-            String password = scanner.nextLine();
-            register.setPassword(password);
+            String password = br.readLine();
+            ps.setString(4, password);
             System.out.print("Enter your HES Code: ");
-            String hesCode = scanner.nextLine();
-            register.setHesCode(hesCode);
+            String hesCode = br.readLine();
+            ps.setString(5, hesCode);
+            int voyagePoints = 0;
+            int isAdmin = 0;
+            ps.setInt(6, voyagePoints);
+            ps.setInt(7, isAdmin);
+            ps.executeUpdate();
             System.out.println("=====================");
             System.out.println("Account created successfully.");
-            System.out.println("Back to the menu now!");
-            System.out.println("=====================");
-            MenuScreen menu = new MenuScreen();
+            LoginScreen.backToMenu();
+            //MenuScreen menu = new MenuScreen();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
         }
     }
-    // PRINTING CREATED USER 
-    private void printRegisteredUser(Registration register) {
-        System.out.println(register.getFirstName());
-        System.out.println(register.getLastName());
-        System.out.println(register.getUsername());
-        System.out.println(register.getPassword());
-        System.out.println(register.getHesCode());
-    } 
+    // // PRINTING CREATED USER 
+    // private void printRegisteredUser(Registration register) {
+    //     System.out.println(register.getFirstName());
+    //     System.out.println(register.getLastName());
+    //     System.out.println(register.getUsername());
+    //     System.out.println(register.getPassword());
+    //     System.out.println(register.getHesCode());
+    // } 
 }
-    // CAN BE USER CLASS LATER 
-    // SETTERS AND GETTERS FOR REGISTRATION OF USER
-    class Registration {
-        private String firstName;
-        private String lastName;
-        private String username;
-        private String password;
-        private String hesCode;
-
-        public String getFirstName() {
-            return this.firstName;
-        }
-
-        public void setFirstName(String firstName) {
-            this.firstName = firstName;
-        }
-
-        public String getLastName() {
-            return this.lastName;
-        }
-
-        public void setLastName(String lastName) {
-            this.lastName = lastName;
-        }
-
-        public String getUsername() {
-            return this.username;
-        }
-
-        public void setUsername(String username) {
-            this.username = username;
-        }
-
-        public String getPassword() {
-            return this.password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
-        public String getHesCode() {
-            return this.hesCode;
-        }
-
-        public void setHesCode(String hesCode) {
-            this.hesCode = hesCode;
-        }
-}
-
