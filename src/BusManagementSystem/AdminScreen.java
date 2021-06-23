566d71c8ea362290eb818dc334cda9c309aec058
Doğancan <dhirdavatcioglu@yahoo.com>
2021-06-24 02:03:45 +0300

    new screens were added & functions were updated

--- src/BusManagementSystem/AdminScreen.java
+++ src/BusManagementSystem/AdminScreen.java
@@ -1,8 +1,10 @@
 package src.BusManagementSystem;
 
 import java.util.Scanner;
+
 // ADMIN SCREEN NAVIGATOR
 public class AdminScreen {
+	
     public AdminScreen() {
         Scanner choice = new Scanner(System.in);
         adminMenu();
@@ -14,16 +16,24 @@ public class AdminScreen {
                 Voyage.deleteVoyage();
                 break;
             case 3:
-                // User.deleteUser();
+                User.deleteUser();
                 break;
             case 4:
-                VoyageScreen.voyageMenu();
+                Voyage.printVoyages();
+                break;
+            case 5:
+            	ConnectionManager.resetDB();
+            	System.out.println("Database reset succesfully!");
+            	break;
+            case 6:
+            	System.out.println("Goodbye!");
                 break;
             default:
                 System.out.println("Error");
                 break;
         }
     }
+    
     // ADMIN SCREEN DISPLAY
     public void adminMenu() {
         System.out.println("WELCOME TO THE BESTBUS ADMIN PAGE");
@@ -32,6 +42,8 @@ public class AdminScreen {
         System.out.println("2. Delete Voyage");
         System.out.println("3. Delete User");
         System.out.println("4. List Voyages");
+        System.out.println("5. Reset Database");
+        System.out.println("6. Exit");
         System.out.println("=====================");
         System.out.println("Enter choice: ");
     }
