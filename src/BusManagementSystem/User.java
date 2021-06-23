7b620fbfcfd5d0a94dec81fdd227b0a97e61ef94
Doğancan <dhirdavatcioglu@yahoo.com>
2021-06-24 01:49:55 +0300

    new functions were added

--- src/BusManagementSystem/User.java
+++ src/BusManagementSystem/User.java
@@ -1,6 +1,12 @@
 package src.BusManagementSystem;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 public class User {
-    // USER SETTERS GETTERS AND VARIABLES
 		private String firstName;
         private String lastName;
         private String username;
@@ -8,15 +14,37 @@ public class User {
         private String hesCode;
         public static boolean isLoggedIn = false;
         public static boolean isAdmin = false;
+        public static String ID ;
+        public static String ticketName;
+        public static int voyagePoints = 0;
 
-        public User(String firstName, String lastName, String username, String password, String hesCode) {
-    		
+        public User(String firstName, String lastName, String username, String password, String hesCode) { 		
     		this.firstName = firstName;
     		this.lastName = lastName;
     		this.username = username;
     		this.password = password;
     		this.hesCode = hesCode;
-    	}
+    	}      
+        
+        // DELETE USER FUNCTION FOR ADMIN
+        public static void deleteUser(){
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            try {
+    	        System.out.println("Please enter userID to delete a user: ");
+    	        int userID = Integer.parseInt(input.readLine());
+            	String sqlQuery = "delete from bmsUsers where userID = '"+userID+"'";
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                Connection con = DriverManager.getConnection("jdbc:mysql://94.130.57.82/appsplat_semih", "appsplat_semih", "semihsemih123");
+                PreparedStatement ps = con.prepareStatement(sqlQuery);
+    	        ps.executeUpdate();
+            }
+            catch (Exception e) {
+            	System.out.println(e);
+            }
+            
+            System.out.println("User Deleted Successfully...");
+            LoginScreen.backToMenu();
+        }
         
         public String getFirstName() {
             return this.firstName;
